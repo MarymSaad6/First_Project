1517e1d11ef558a9f933a56424c0224992a9d7a1
MarymSaad6 <maryam889898@gmail.com>
2022-10-19 15:50:32 +0200

    c++

---
+++ maryam.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string arr[3]={"Damietta" , "Assuit", "Cairo" };
+	for(int i=0;i<3;i++)
+	{
+		cout<< arr[i]<<endl;
+	}
+	system ("pause");
+	return 0;
+	
+}
